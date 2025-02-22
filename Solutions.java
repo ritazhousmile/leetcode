ffad554cebe464f0a2a15f6ce3c0515184cd9eb4
ritazhousmile <ritazhousmile@gmail.com>
2025-02-22 13:23:37 -0500

    two sum solutions

--- Solutions.java
+++ Solutions.java
@@ -18,9 +18,36 @@ Output: [1,2]
 
  */
 
+import java.util.HashMap;
+
 public class Solutions {
+     //o(n*n)solution
     public int[] twoSum(int[] nums, int target) {
-        
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[] {i, j};
+                }
+            }
+        }
+        return new int[] {};
     }
     
+    //o(n)solution
+    public int[] twoSum2(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+
+            map.put(nums[i], i);
+        }
+        return null;
+    }
 }
+
+
